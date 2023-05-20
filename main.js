6b18fb8deeadd8ec91efc7cb9e06b477f67f5598
SeherS20 <99300690+SeherS20@users.noreply.github.com>
2023-05-20 18:29:52 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,37 @@ function start(){
 function modelReady() {
     classifier.classify(gotResult)
 }
+
+function gotResult(error,results){
+    if(error){
+        console.error(error)
+    }
+    else {
+        console.log(results)
+        random_number_R = Math.floor(Math.random() * 255)+1
+        random_number_G = Math.floor(Math.random() * 255)+1
+        random_number_B = Math.floor(Math.random() * 255)+1
+        document.getElementById("result_label").style.color = "rgb("+random_number_R+" , "+random_number_G+" , "+random_number_B+")"
+        document.getElementById("accuracy_label").style.color = "rgb("+random_number_R+" , "+random_number_G+" , "+random_number_B+")"
+        
+        document.getElementById("result_label").innerHTML = "I can hear the sound of " + results[0].label
+        document.getElementById("accuracy_label").innerHTML = "I am " + (results[0].confidence * 100).toFixed(2) + "% sure!" 
+
+        pic = document.getElementById("sound")
+
+        if(results[0].label == "Background Noise"){
+            pic.src = 'ear.png';
+        }
+        else if(results[0].label == "cat"){
+            pic.src = 'cat ears.png';
+        }
+        else if(results[0].label == "dog"){
+            pic.src = 'DOGGO.png';
+        }
+        else if(results[0].label == "human"){
+            pic.src = 'human.png';
+        }
+    }
+
+
+}
